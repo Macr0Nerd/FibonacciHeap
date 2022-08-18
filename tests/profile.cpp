2805414f2a0fa9fcb6b7ca79ea45c2031e04d8d5
Gabriele A. Ron <macrocode@yahoo.com>
2022-08-17 20:21:39 -0500

    Added executable to profile

---
+++ tests/profile.cpp
@@ -0,0 +1,40 @@
+#include "GRon/FibonacciHeap/FibonacciHeap.hpp"
+
+#include <iostream>
+
+int main() {
+    int n = 10000000;
+    int ratio = 128;
+    bool invert = false;
+
+    int one = n/100;
+
+    std::cout << "n=" << n << ", ratio=" << ratio << ", invert=" << invert << std::endl;
+    std::cout << "=======================================================" << std::endl;
+
+    int pop_cnt = 0, insert_cnt = 0;
+
+    FibonacciHeap<int> test;
+    for (int i = 0; i < n; i++) {
+        if (!(i % one)) {
+            std::cout << '\r' << (i/(double) n) * 100 << "% Complete" << std::flush;
+        }
+
+        if (!(rand() % ratio) ^ invert) {
+            test.pop_minimum();
+            pop_cnt++;
+            continue;
+        }
+
+        test.insert(rand());
+        insert_cnt++;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Results" << std::endl;
+    std::cout << "==============" << std::endl;
+    std::cout << "Pop: " << pop_cnt << " ops" << std::endl;
+    std::cout << "Insert: " << insert_cnt << " ops" << std::endl;
+
+    return 0;
+}
