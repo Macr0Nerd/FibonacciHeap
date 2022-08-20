4afe156bc096999c59098d777933e3e035e20bb2
Gabriele A. Ron <macrocode@yahoo.com>
2022-08-20 02:11:28 -0500

    :'(

--- benchmark/README.md
+++ benchmark/README.md
@@ -4,3 +4,12 @@ These benchmarks are used to test the speed of the Fibonacci Heap implemented in
 ## Building
 First set up the external dependency `CppBenchmark` in `extern` (it's a git submodule so I can't automate the building of it, sorry), and then build the benchmarks in the same way as with the main project.
 Refer to [CppBenchmark](https://github.com/chronoxor/CppBenchmark) for more info on how the benchmarks work.
+
+## Set Benchmark Type
+When you run the first cmake command, run it with:
+
+    -DBENCHMARK_TYPE=TYPE
+
+where `TYPE` is either:
+* INT
+* STR
---
+++ benchmark/app/string.cpp
@@ -0,0 +1,86 @@
+#include "GRon/FibonacciHeap/FibonacciHeap.hpp"
+#include "benchmark/cppbenchmark.h"
+
+#include <functional>
+#include <queue>
+#include <string>
+
+#define run_inf CppBenchmark::Settings().Infinite().Attempts(50)
+#define N 1000000
+#define RATIO 65536
+
+namespace GRon {
+    template<typename T>
+    class priority_queue_mfh : public FibonacciHeap<T> {
+    public:
+
+        priority_queue_mfh() = default;
+        priority_queue_mfh(size_t n) : FibonacciHeap<T>(n) {}
+
+        [[nodiscard]] typename FibonacciHeap<T>::Node& top() {
+            return *this->get_minimum();
+        }
+
+        [[nodiscard]] bool empty() const {
+            return this->location.empty();
+        }
+
+        void push(const T& value) {
+            this->insert(value);
+        }
+
+        void pop() {
+            this->pop_minimum();
+        }
+    };
+}
+
+template<typename T>
+class HeapFixture : public virtual CppBenchmark::Fixture {
+protected:
+    T heap;
+
+    void Initialize(CppBenchmark::Context& context) override { heap = T(); }
+    void Cleanup(CppBenchmark::Context& context) override { heap = T(); }
+};
+
+template<typename T>
+class HeapParamFixture : public virtual CppBenchmark::Fixture {
+protected:
+    T heap;
+
+    void Initialize(CppBenchmark::Context& context) override { heap = T(static_cast<size_t>(context.x())); }
+    void Cleanup(CppBenchmark::Context& context) override { heap = T(); }
+};
+
+template<class T>
+void run(T& heap, size_t n, int ratio, bool invert = false) {
+    for (int i = 0; i < n; ++i) {
+        if (!(i % ratio) ^ invert) {
+            heap.top();
+            continue;
+        }
+
+        heap.push(std::to_string(i));
+    }
+}
+
+/**
+ * GRon::priority_queue_mfh
+ */
+
+BENCHMARK_FIXTURE(HeapParamFixture<GRon::priority_queue_mfh<std::string>>, "pqmfh", run_inf.Param(N)) {
+    run(heap, N, RATIO);
+    context.Cancel();
+}
+
+/**
+ * std::priority_queue
+ */
+
+BENCHMARK_FIXTURE(HeapFixture<std::priority_queue<std::string>>, "std::pq", run_inf) {
+    run(heap, N, RATIO);
+    context.Cancel();
+}
+
+BENCHMARK_MAIN()
